26dc3c3029cab98a94fa60776cf24c332fbe7bdc
Muthamilarasan <muthamilarasan.d@cognizant.com>
2018-07-13 11:06:38 +0530

    Update package.sh

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,3 +1,48 @@
-#!/usr/bin/env bash
-echo "Building................"
+#!/bin/sh
+
+set -e +x
+
+pushd source-code
+  echo "Packaging JAR"
+  ./mvnw clean package -DskipTests
+popd
+
+jar_count=`find source-code/target -type f -name *.jar | wc -l`
+
+if [ $jar_count -gt 1 ]; then
+  echo "More than one jar found, don't know which one to deploy. Exiting"
+  exit 1
+fi
+
+find source-code/target -type f -name *.jar -exec cp "{}" package-output/application.jar \;
+
+echo "Done packaging"
 exit 0
+
+#!/usr/bin/env bash
+
+set -ex
+echo "Building................"
+pushd petclinic-sourcecode
+  export TERM=dumb
+
+    pwd
+    ls -lart
+
+    mvn clean install -e -DskipTests=true
+
+    echo "Moving binary ................"
+    pwd
+    cd spring-petclinic-server
+    ls -lart
+    cd target
+    ls -lart
+    mv *.jar ../../../jarFile/
+    cd ../../../jarFile
+    ls -lart
+    cd ..
+    ls -lart
+
+
+popd
+echo "Build Completed !!!"
